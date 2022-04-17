e85db46ea623383b7605c393e4c27e8d1f8972e6
Nikita Tulenev <n1tulenev@gmail.com>
2022-04-17 13:03:05 +0300

    Add test project

---
+++ TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092",
+    CompressionType = CompressionType.Gzip
+};
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+await producer.ProduceAsync("test", new Message<string, string>
+{
+    Key = "test",
+    Value = "value"
+});
